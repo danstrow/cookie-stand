c7aa7a8c82fe0ad9012d8135d6cee3cebd032e1b
Daniel <danielwesleyolson@comcast.net>
2019-02-14 19:44:50 -0800

    progress made 744 Thursday

---
+++ js/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var hours = [
+  '6 am',
+  '7 am',
+  '8 am',
+  '9 am',
+  '10 am',
+  '11 am',
+  '12 pm',
+  '1 pm',
+  '2 pm',
+  '3 pm',
+  '4 pm',
+  '5 pm',
+  '6 pm',
+];
+
+var firstandpikeUl = document.getElementById('firstandpike');
+var seaintlUl = document.getElementById('seaintl');
+var seattlecenterUl = document.getElementById('seattlecenter');
+var caphillUl = document.getElementById('caphill');
+var alkiUl = document.getElementById('alki');
+
+
+var firstandpike = {
+  name: 'firstandpike', 
+  minimum: '23',
+  maximum: '65',
+  average: '6.3',
+ 
+  render: function() {
+    // for(var i = 0; i < hours.length; i++) {
+    //   var liEl = document.createElement('li');
+    //   liEl.textContent = `${hours[i]}: ${this.shopStats[i]} stats`;
+    //   firstandpikeUl.appendChild(liEl);
+    console.log("testtest");
+    }
+  }
+
+// };
+firstandpike.render();
